50db7477c8644bf5a1c790609fdac8eb457452a6
尐 <313303303@qq.com>
2018-08-23 09:56:07 +0800

    Update reader_pandas.py

--- reader_pandas.py
+++ reader_pandas.py
@@ -1,27 +1,18 @@
 import time
 import pandas as pd
+from tqdm import tqdm
 
-
-def execution_time(func):
-    def wrapper(*args, **kwargs):
-        t1 = time.time()
-        tmp = func(*args, **kwargs)
-        t2 = time.time()
-        t = t2 - t1
-        print("""Executing Time of "%s": %.3f s""" % (func.__name__, t))
-        return tmp
-    return wrapper
 
 @execution_time
-def reader_pandas(file, chunkSize=10000):
+def reader_pandas(file, chunkSize=100000, patitions=10 ** 4):
     reader = pd.read_csv(file, iterator=True)
-    chunks =[]
-    while 1:
-        try:
-            chunk = reader.get_chunk(chunkSize)
-            chunks.append(chunk)
-        except StopIteration:
-            break
+    chunks = []
+    with tqdm(range(patitions), 'Reading ...') as t:
+        for _ in t:
+            try:
+                chunk = reader.get_chunk(chunkSize)
+                chunks.append(chunk)
+            except StopIteration:
+                break
     return pd.concat(chunks, ignore_index=True)
-
 
